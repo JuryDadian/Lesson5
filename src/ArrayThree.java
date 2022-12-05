b517b7014a170f4b9e3eaf33c061bccb2492a550
JuryDadian <filejob@mail.ru>
2022-12-05 20:16:41 +0300

    1

--- src/ArrayThree.java
+++ src/ArrayThree.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ArrayThree {
+import java.util.Scanner;
+
+public class ArrayThree {
+    public static void main(String[] args) {
+        int[][][] array = {{{1, 2, 3},{10, 20, 30, 40}}, {{500, 600}}};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = sc.nextInt();
+        for (int[][] arr1: array) {
+            for (int[]arr2: arr1) {
+                for (int x: arr2) {
+                    System.out.print(x + a +" ");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
